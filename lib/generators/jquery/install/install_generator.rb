469824e11a6edb5f6522f23062e505daacbdcaf1
unknown <gomezn@.am.corp.amgen.com>
2010-10-04 13:16:20 -0700

    Added option for jquery-ui version

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -5,7 +5,8 @@ module Jquery
       class_option :ui, :type => :boolean, :default => false, :desc => "Whether to Include JQueryUI"
       class_option :version, :type => :string, :default => "1.4.1", :desc => "Which version of JQuery to fetch"
       @@versions = %w( 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
-
+      class_option :ui-version, :type => :string, :default => "1.8.5", :desc => "Which version of JQuery UI to fetch"
+      @@ui-versions = %w( 1.8.4 1.8.5 )
 
       def remove_prototype
         %w(controls.js dragdrop.js effects.js prototype.js).each do |js|
@@ -25,10 +26,20 @@ module Jquery
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[1]}/jquery.js", "public/javascripts/jquery.js"
         end
 
+
         # Downloading latest jQueryUI minified
-        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js" if options.ui?
-        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.js", "public/javascripts/jquery-ui.js" if options.ui?
-      end
+        if options.ui?
+          if @@ui-versions.include?(options.ui-version)
+            puts "Fetching JQuery UI version #{options.ui-version}!"
+            get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{options.ui-version}/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
+            get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{options.ui-version}/jquery-ui.js", "public/javascripts/jquery-ui.js"
+          else
+            puts "JQuery UI #{options.ui-version} is invalid; fetching #{@@ui-versions[1]} instead."
+            get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{@@ui-versions[1]}/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
+            get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{@@ui-versions[1]}/jquery-ui.js", "public/javascripts/jquery-ui.js"
+          end
+        end
+     end
 
       def download_ujs_driver
         # Downloading latest jQuery drivers
