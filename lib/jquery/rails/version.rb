5724936a6a57c499213631cb1d8f0693d6d30ba0
Thiago Almeida <tapgyn@gmail.com>
2012-02-05 14:33:37 -0200

    making it version 1.0.18 for jquery 1.7.1

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,6 +1,6 @@
 module Jquery
   module Rails
-    VERSION = "1.0.17"
+    VERSION = "1.0.18"
     JQUERY_VERSION = "1.7.1"
     JQUERY_UI_VERSION  = "1.8.17"
     JQUERY_UJS_VERSION = "17f4004310b6ece3cb240914932b4d6d46032c24"
