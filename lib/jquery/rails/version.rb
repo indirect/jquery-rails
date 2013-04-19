dd031f89c7f077df98cee6bdbde0d30ad842b244
Chris Sloan <chris@chrissloan.info>
2013-04-19 09:43:13 -0300

    Updating to 2.0.3

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
-    VERSION = "2.0.2"
-    JQUERY_VERSION = "1.7.2"
-    JQUERY_UI_VERSION  = "1.8.18"
-    JQUERY_UJS_VERSION = "caabad7f7ba6c35de1588ea263295ed5bc166a43"
+    VERSION = "2.0.3"
+    JQUERY_VERSION = "2.0.0"
+    JQUERY_UI_VERSION  = "1.10.2"
+    JQUERY_UJS_VERSION = "610dfb8881377598b7e63ab2c152d73aa1d3632f"
   end
 end
